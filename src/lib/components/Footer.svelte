56d41a72946dcf0960c59db84653c22971118077
Hadeel <hadelturki@hotmail.com>
2024-08-18 22:19:59 +0300

    coe

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Button from "$lib/components/ui/button/button.svelte";
+</script>
+
+<footer class=" bg-secondary text-secondary-foreground  p-6 p ">
+    <div class="max-w-screen-2xl flex justify-end items-center mx-auto gap-5">
+            <span class="flex flex-col items-center">
+                <span class="font-bold text-sm">
+                    Email 
+                </span>
+                <a class="text-xs" href="mailto:hadelturki@hotmail.com"
+                    >hadelturki@hotmail
+                </a>
+            </span>
+            
+
+        <div class="flex flex-col items-center">
+            <span class="text-sm font-bold "> © {new Date().getFullYear()}          </span>
+                <span class="text-xs">by Hadeel</span>  
+        </div>
+      </div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,11 @@
     import Navbar from "$lib/components/navbar.svelte";
 import "../app.css";
     import { ModeWatcher } from "mode-watcher";
+    import Footer from "$lib/components/Footer.svelte"
 </script>
 <Navbar/>
 
 <ModeWatcher />
 <slot></slot>
+<Footer/>
 
